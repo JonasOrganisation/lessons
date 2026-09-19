4d41ae859a942f558496741caa84ccb83972a24f
Jonas Moreau <jonas.moreau@hotmail.com>
2026-09-19 09:58:16 +0200

    featur:'add algo.html' + fix:'overflow-x in body'

--- components/header.js
+++ components/header.js
@@ -2,36 +2,35 @@ class SiteHeader extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
 <header class="padding">
-<a class="logo" href="./index.html">
-<img src="./assets/images/logo.png" alt="Logo" />
-</a>
-<div class="header-wrapper">
-<nav>
-    <ul>
-    <li>
-        <a href="#">Accueil</a>
-    </li>
-    <li>
-        <a href="#">À propos</a>
-    </li>
-    <li>
-        <a href="#">Contact</a>
-    </li>
-    </ul>
-</nav>
-</div>
-<button
-class="hamburger"
-type="button"
-aria-label="Ouvrir le menu"
-aria-expanded="false"
-aria-controls="menu-principal"
->
-<div class="top-bun" aria-hidden="true"></div>
-<div class="meat" aria-hidden="true"></div>
-<div class="bottom-bun" aria-hidden="true"></div>
-</button>
-</header>;
+  <a class="logo" href="./index.html">
+    <img src="./assets/images/logo.png" alt="Logo" />
+  </a>
+
+  <div class="header-wrapper">
+    <nav>
+      <ul>
+        <li>
+          <a href="/index.html">Accueil</a>
+        </li>
+        <li>
+          <a href="/algo.html">Mini-algorithmes</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+
+  <button
+    class="hamburger"
+    type="button"
+    aria-label="Ouvrir le menu"
+    aria-expanded="false"
+    aria-controls="menu-principal"
+  >
+    <div class="top-bun" aria-hidden="true"></div>
+    <div class="meat" aria-hidden="true"></div>
+    <div class="bottom-bun" aria-hidden="true"></div>
+  </button>
+</header>
 `;
   }
 }
